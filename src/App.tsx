820648e460dd38eef33f344550a6b7ab6471f9fc
GuhSntLean <guleandro.66@gmail.com>
2024-02-24 17:25:49 -0300

    populando a tabela de informações e criando a paginação da tabela

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,62 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
 } from "@mui/material";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+interface Character {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: [string];
+  species: [string];
+  vehicles: [string];
+  starships: [string];
+  created: string;
+  edited: string;
+  url: string;
+}
 
 function App() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  // Paginação
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(3);
+
+  useEffect(() => {
+    const listPersons = async () => {
+      const dataFetch = await fetch("https://swapi.dev/api/people");
+      const response = await dataFetch.json();
+
+      setCharacters(response.results);
+      console.log(characters);
+    };
+    listPersons();
+  });
+
+  // Paginação
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+
+
   return (
     <Box sx={{ width: "100%" }}>
       <Paper sx={{ width: "100%", mb: 2 }}>
@@ -18,38 +69,35 @@ function App() {
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>Campo 1</TableCell>
-                <TableCell>Campo 2</TableCell>
-                <TableCell>Campo 3</TableCell>
-                <TableCell>Campo 4</TableCell>
-                <TableCell>Campo 5</TableCell>
+                <TableCell>Nome</TableCell>
+                <TableCell>Altura</TableCell>
+                <TableCell>Cor do cabelo</TableCell>
+                <TableCell>Cor dos olhos</TableCell>
+                <TableCell>Data de aniversario</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              <TableRow>
-                <TableCell>Campo 1</TableCell>
-                <TableCell>Campo 2</TableCell>
-                <TableCell>Campo 3</TableCell>
-                <TableCell>Campo 4</TableCell>
-                <TableCell>Campo 5</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Campo 1</TableCell>
-                <TableCell>Campo 2</TableCell>
-                <TableCell>Campo 3</TableCell>
-                <TableCell>Campo 4</TableCell>
-                <TableCell>Campo 5</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Campo 1</TableCell>
-                <TableCell>Campo 2</TableCell>
-                <TableCell>Campo 3</TableCell>
-                <TableCell>Campo 4</TableCell>
-                <TableCell>Campo 5</TableCell>
-              </TableRow>
+              {characters.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((character) => (
+                <TableRow>
+                  <TableCell>{character.name}</TableCell>
+                  <TableCell>{character.height}</TableCell>
+                  <TableCell>{character.hair_color}</TableCell>
+                  <TableCell>{character.eye_color}</TableCell>
+                  <TableCell>{character.birth_year}</TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[3, 5, 10]}
+          component="div"
+          count={characters.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </Paper>
     </Box>
   );
