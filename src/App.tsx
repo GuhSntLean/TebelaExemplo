2220f970cf9aeafa22f5743ab944625b0d912959
GuhSntLean <guleandro.66@gmail.com>
2024-02-24 10:46:59 -0300

    configuração e instalação de dependencia do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Campo 1</TableCell>
+                <TableCell>Campo 2</TableCell>
+                <TableCell>Campo 3</TableCell>
+                <TableCell>Campo 4</TableCell>
+                <TableCell>Campo 5</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Campo 1</TableCell>
+                <TableCell>Campo 2</TableCell>
+                <TableCell>Campo 3</TableCell>
+                <TableCell>Campo 4</TableCell>
+                <TableCell>Campo 5</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Campo 1</TableCell>
+                <TableCell>Campo 2</TableCell>
+                <TableCell>Campo 3</TableCell>
+                <TableCell>Campo 4</TableCell>
+                <TableCell>Campo 5</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Campo 1</TableCell>
+                <TableCell>Campo 2</TableCell>
+                <TableCell>Campo 3</TableCell>
+                <TableCell>Campo 4</TableCell>
+                <TableCell>Campo 5</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Box>
   );
 }
 
